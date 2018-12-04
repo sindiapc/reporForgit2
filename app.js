cf8448b8a2f01fcecf07395656fcdd5c8bf3e5bb
Sindia Choolackal <sindiapc@gmail.com>
2018-12-04 19:05:06 +0530

    Added squaring arrow function

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 "use strict";
 var myName = "John Galt";
 console.log(myName);
+//arrow function
+var getSquare = function (a, b) {
+    console.log("Finding the square of ", a, "+", b);
+    return (a * a) + (b * b) + (2 * a * b);
+};
+console.log(getSquare(5, 3));
--- app.ts
+++ app.ts
@@ -1,2 +1,10 @@
 let myName:string="John Galt"
 console.log(myName);
+//arrow function
+
+const getSquare =(a:number,b:number):number=>{
+    console.log("Finding the square of ",a,"+",b);
+    return (a*a)+(b*b)+(2*a*b);
+    
+}
+console.log(getSquare(5,3));
